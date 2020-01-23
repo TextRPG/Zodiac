e5c0de80ff289efb6fe3d0ea7610b5eccc928722
aangairbender <jetixranger@ya.ru>
2020-01-24 01:40:39 +0900

    basic ui added

--- Zodiac.Common/Game1.cs
+++ Zodiac.Common/Game1.cs
@@ -10,7 +10,12 @@ namespace Zodiac.Common
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Texture2D tex;
+        Texture2D villageBg;
+        Texture2D waterHouseBg;
+
+        SpriteFont header;
+        SpriteFont listItem;
+
         RenderTarget2D renderTarget;
 
         public Game1()
@@ -37,7 +42,10 @@ namespace Zodiac.Common
         {
             renderTarget = new RenderTarget2D(GraphicsDevice, 720, 1280);
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            tex = Content.Load<Texture2D>("char");
+            villageBg = Content.Load<Texture2D>("village_bg");
+            waterHouseBg = Content.Load<Texture2D>("water_house_bg");
+            header = Content.Load<SpriteFont>("header");
+            listItem = Content.Load<SpriteFont>("list_item");
         }
 
         protected override void UnloadContent()
@@ -62,12 +70,56 @@ namespace Zodiac.Common
             spriteBatch.Draw(tex, Vector2.Zero, Color.White);
             spriteBatch.End();*/
 
-            var rect = new Image { Left = 50, Top = 50, Width = 100, Height = 200, Background = Color.Gray, Texture = tex };
-            rect.Draw(spriteBatch);
+            var label = new Label {
+                Left = 0, Top = 0,
+                Width = 720, Height = 75,
+                Background = new Color(27, 38, 44),
+                Caption = "Village",
+                Color = new Color(187, 225, 250),
+                Font = header };
+            label.Draw(spriteBatch);
+
+            var image = new Image { Left = 0, Top = label.Height, Width = 720, Height = 450, Background = Color.Transparent, Texture = villageBg };
+            image.Draw(spriteBatch);
+
+            var items = new[]
+            {
+                "Waterhouse",
+                "Field",
+                "My house"
+            };
+
+            var itemSeprator = new Control { Left = 0, Top = image.Top + image.Height, Width = 720, Height = 10, Background = new Color(50, 130, 184) };
+            itemSeprator.Draw(spriteBatch);
+            var itemControl = new Control { Left = 0, Top = itemSeprator.Top + itemSeprator.Height, Width = 720, Height = 100, Background = new Color(15, 76, 117)};
+            itemSeprator.Top += itemSeprator.Height + itemControl.Height;
+
+            for (int i = 0; i < items.Length; ++i)
+            {
+                itemControl.Draw(spriteBatch);
+                itemSeprator.Draw(spriteBatch);
+
+                var text = new Label
+                {
+                    Left = 0,
+                    Top = itemControl.Top,
+                    Width = 720,
+                    Height = itemControl.Height,
+                    Background = Color.Transparent,
+                    Caption = items[i],
+                    Color = new Color(187, 225, 250),
+                    Font = listItem
+                };
+                text.Draw(spriteBatch);
+
+                itemControl.Top += itemControl.Height + itemSeprator.Height;
+                itemSeprator.Top += itemControl.Height + itemSeprator.Height;
+            }
+
 
             GraphicsDevice.SetRenderTarget(null);
             spriteBatch.Begin();
-            spriteBatch.Draw(renderTarget, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+            spriteBatch.Draw(renderTarget, GraphicsDevice.Viewport.Bounds, Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ Zodiac.Common/UI/Label.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zodiac.Common.UI
+{
+    public class Label : Control
+    {
+        public SpriteFont Font { get; set; }
+
+        public string Caption { get; set; }
+
+        public Color Color { get; set; }
+
+        protected override void Render(SpriteBatch spriteBatch)
+        {
+            var sz = Font.MeasureString(Caption);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(Font, Caption, new Vector2(Left + (Width - sz.X) / 2f, Top + (Height - sz.Y) / 2f), Color);
+            spriteBatch.End();
+        }
+    }
+}
